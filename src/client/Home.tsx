d497c601d309bd61ec8d0b25c13f4080c22c067a
Dexx <dexx.van.der.mandele@mendix.com>
2019-03-12 17:22:50 +0100

    Remove redundant debug code

--- src/client/Home.tsx
+++ src/client/Home.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Alert, StyleSheet, View } from "react-native";
 
-import { SetCredentialsDialog } from "./dialogs/SetCredentialsDialog";
 import { SpamSkillDialog } from "./dialogs/SpamSkillDialog";
 import { TileButton } from "./TileButton";
 
 import { listItems } from "../items/listItems";
-import { getCredentials } from "../store/CredentialStore";
-import { getUserData } from "../userData/userData";
 
 export default class Home extends React.Component {
     state = {
         spamSkillDialogVisible: false,
-        setCredentialsDialogVisible: false,
     };
 
     render() {
@@ -20,31 +16,18 @@ export default class Home extends React.Component {
                 <View style={styles.container}>
                     <View style={styles.column}>
                         <TileButton
-                            text="Habit"
-                            onPress={this.getUserHabit}
-                        />
-                        <TileButton
-                            text="List 'em"
+                            text="Feed pets"
                             onPress={this.listPets}
                         />
                     </View>
 
                     <View style={styles.column}>
                         <TileButton
-                            text="Burst of Flames"
+                            text="Use Skill"
                             onPress={this.toggleSpamSkillDialog}
-                        />
-                        <TileButton
-                            text="Set credentials"
-                            onPress={this.toggleSetCredentialsDialog}
-                        />
-                        <TileButton
-                            text="Get credentials"
-                            onPress={this.getCredentials}
                         />
                     </View>
                     {this.renderSpamSkillDialog()}
-                    {this.renderSetCredentialsDialog()}
                 </View>
         );
     }
@@ -55,18 +38,6 @@ export default class Home extends React.Component {
             : null;
     }
 
-    private renderSetCredentialsDialog() {
-        return this.state.setCredentialsDialogVisible
-            ? <SetCredentialsDialog close={this.toggleSetCredentialsDialog}/>
-            : null;
-    }
-
-    private getUserHabit = async () => {
-        getUserData().then(userData => {
-            Alert.alert("Response", JSON.stringify(userData.tasks.habits[0].text));
-        });
-    }
-
     private listPets = () => {
         listItems("pets").then(items => {
             Alert.alert("Response", JSON.stringify(items));
@@ -75,15 +46,6 @@ export default class Home extends React.Component {
 
     private toggleSpamSkillDialog = () => {
         this.setState({ spamSkillDialogVisible: !this.state.spamSkillDialogVisible });
-    }
-
-    private toggleSetCredentialsDialog = () => {
-        this.setState({ setCredentialsDialogVisible: !this.state.setCredentialsDialogVisible });
-    }
-
-    private getCredentials = async () => {
-        const credentials = await getCredentials();
-        Alert.alert("Credentials?", `id: ${credentials.userId} token: ${credentials.apiToken}`);
     }
 }
 
