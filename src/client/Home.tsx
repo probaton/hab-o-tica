cd5680a9dee17fb4b9ce403ecab53e5c53c31acb
Dexx <dexx.van.der.mandele@mendix.com>
2019-03-28 09:28:15 +0100

    Store user data in home state and pass to dialogs

--- src/client/Home.tsx
+++ src/client/Home.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
+import { IHabiticaData } from "../userData/IHabiticaData";
+import { getUserData } from "../userData/userData";
 import { FeedPetDialog } from "./dialogs/FeedPetDialog";
 import { SpamSkillDialog } from "./dialogs/SpamSkillDialog";
 import { TileButton } from "./TileButton";
 
-export default class Home extends React.Component {
-    state = {
-        spamSkillDialogVisible: false,
-        feedPetDialogVisible: false,
-    };
+interface IHomeState {
+    userData: { lastUpdate: number, data: Promise<IHabiticaData> };
+    openDialog: "feedPet" | "spamSkill" | undefined;
+}
+
+export default class Home extends React.Component<any, IHomeState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            userData: { lastUpdate: new Date().getTime(), data: getUserData() },
+            openDialog: undefined,
+        };
+    }
 
     render() {
         return (
@@ -17,14 +27,14 @@ export default class Home extends React.Component {
                     <View style={styles.column}>
                         <TileButton
                             text="Feed Pet"
-                            onPress={this.toggleFeedPetDialog}
+                            onPress={this.openFeedPetDialog}
                         />
                     </View>
 
                     <View style={styles.column}>
                         <TileButton
                             text="Use Skill"
-                            onPress={this.toggleSpamSkillDialog}
+                            onPress={this.openSpamSkillDialog}
                         />
                     </View>
                     {this.renderSpamSkillDialog()}
@@ -33,24 +43,37 @@ export default class Home extends React.Component {
         );
     }
 
+    private refreshUserData() {
+        const now = new Date().getTime();
+        if (now - this.state.userData.lastUpdate > 180000) {
+            this.setState({ userData: { lastUpdate: now, data: getUserData() } });
+        }
+    }
+
     private renderSpamSkillDialog() {
-        return this.state.spamSkillDialogVisible
-            ? <SpamSkillDialog close={this.toggleSpamSkillDialog}/>
+        return this.state.openDialog === "spamSkill"
+            ? <SpamSkillDialog userData={this.state.userData.data} close={this.closeDialogs}/>
             : null;
     }
 
-    private toggleSpamSkillDialog = () => {
-        this.setState({ spamSkillDialogVisible: !this.state.spamSkillDialogVisible });
+    private openSpamSkillDialog = () => {
+        this.refreshUserData();
+        this.setState({ openDialog: "spamSkill" });
     }
 
     private renderFeedPetDialog() {
-        return this.state.feedPetDialogVisible
-            ? <FeedPetDialog close={this.toggleFeedPetDialog}/>
+        return this.state.openDialog === "feedPet"
+            ? <FeedPetDialog userData={this.state.userData.data} close={this.closeDialogs}/>
             : null;
     }
 
-    private toggleFeedPetDialog = () => {
-        this.setState({ feedPetDialogVisible: !this.state.feedPetDialogVisible });
+    private openFeedPetDialog = () => {
+        this.refreshUserData();
+        this.setState({ openDialog: "feedPet" });
+    }
+
+    private closeDialogs = () => {
+        this.setState({ openDialog: undefined });
     }
 }
 
