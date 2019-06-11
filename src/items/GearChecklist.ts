5e964b337501e4dad532e0a249794b9795719ba7
Dexx <dexx.van.der.mandele@mendix.com>
2019-06-11 13:27:13 +0200

    Simplify GearChecklist

--- src/items/GearChecklist.ts
+++ src/items/GearChecklist.ts
@@ -1,21 +1,10 @@
 export default class GearChecklist {
-    armor: boolean;
-    headGear: boolean;
-    offHand: boolean;
-    bodyAccessory: boolean;
-    mainHand: boolean;
-    eyewear: boolean;
-    headAccessory: boolean;
-    backAccessory: boolean;
-
-    constructor() {
-        this.armor = true;
-        this.headGear = true;
-        this.offHand = true;
-        this.bodyAccessory = true;
-        this.mainHand = true;
-        this.eyewear = true;
-        this.headAccessory = true;
-        this.backAccessory = true;
-    }
+    armor = true;
+    headGear = true;
+    offHand = true;
+    bodyAccessory = true;
+    mainHand = true;
+    eyewear = true;
+    headAccessory = true;
+    backAccessory = true;
 }
