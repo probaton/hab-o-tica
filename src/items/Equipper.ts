1a8372748e0a352689560feea509dc977f69641f
Dexx <dexx.van.der.mandele@mendix.com>
2019-06-21 15:42:11 +0200

    Also equip mount and pet

--- src/items/Equipper.ts
+++ src/items/Equipper.ts
@@ -6,35 +6,48 @@ export default class Equipper {
     gearType: "equipped" | "costume";
     newOutfit: IOutfit;
     currentOutfit: IOutfit;
-    failMessage = "";
 
     constructor(outfit: IOutfit, useCostume: boolean, userData: IHabiticaData) {
         this.gearType = useCostume ? "costume" : "equipped";
         this.newOutfit = outfit;
         const gear = userData.items.gear;
-        this.currentOutfit = { name: "current", gearSet: useCostume ? gear.costume : gear.equipped };
+        this.currentOutfit = {
+            name: "current",
+            gearSet: useCostume ? gear.costume : gear.equipped,
+            skin: userData.preferences.skin,
+            background: userData.preferences.background,
+            pet: userData.items.currentPet,
+            mount: userData.items.currentMount,
+        };
     }
 
     async equipAll(): Promise<string> {
-        await this.equipItem("armor", this.newOutfit.gearSet.armor);
-        await this.equipItem("head", this.newOutfit.gearSet.head);
-        await this.equipItem("shield", this.newOutfit.gearSet.shield);
-        await this.equipItem("body", this.newOutfit.gearSet.body);
-        await this.equipItem("weapon", this.newOutfit.gearSet.weapon);
-        await this.equipItem("eyewear", this.newOutfit.gearSet.eyewear);
-        await this.equipItem("headAccessory", this.newOutfit.gearSet.headAccessory);
-        await this.equipItem("back", this.newOutfit.gearSet.back);
-        return this.failMessage || `${this.newOutfit.name} equipped successfully.`;
+        const failMessage = [
+            await this.equipItem("armor"),
+            await this.equipItem("head"),
+            await this.equipItem("shield"),
+            await this.equipItem("body"),
+            await this.equipItem("weapon"),
+            await this.equipItem("eyewear"),
+            await this.equipItem("headAccessory"),
+            await this.equipItem("back"),
+            await this.equip("pet", this.newOutfit.pet, this.currentOutfit.pet),
+            await this.equip("mount", this.newOutfit.mount, this.currentOutfit.mount),
+        ].filter(Boolean).join("\n");
+        return failMessage || `${this.newOutfit.name} equipped successfully.`;
     }
 
-    async equipItem(slot: "armor" | "head" | "shield" | "body" | "weapon" | "eyewear" | "headAccessory" | "back", item?: string): Promise<void> {
-        const currentItem = this.currentOutfit.gearSet[slot];
-        if (item && item !== currentItem) {
-            const newItem = item.endsWith("_base_0") ? currentItem : item; // Unequip current item if the outfit slot is explicitly empty.
+    async equipItem(type: "armor" | "head" | "shield" | "body" | "weapon" | "eyewear" | "headAccessory" | "back" ): Promise<string | undefined> {
+        return await this.equip(this.gearType, this.newOutfit.gearSet[type], this.currentOutfit.gearSet[type]);
+    }
+
+    async equip(type: "mount" | "pet" | "costume" | "equipped", newItem?: string, currentItem?: string): Promise<string | undefined> {
+        if (newItem && newItem !== currentItem) {
+            const toBeEquipped = newItem.endsWith("_base_0") ? currentItem : newItem; // Unequip current item if the outfit slot is explicitly empty.
             try {
-                await callHabApi(`/api/v3/user/equip/${this.gearType}/${newItem}`, "POST");
+                await callHabApi(`/api/v3/user/equip/${type}/${toBeEquipped}`, "POST");
             } catch (e) {
-                this.failMessage += `Failed to equip ${item}: ${e.message}\n`;
+                return `Failed to (un)equip ${newItem}: ${e.message}`;
             }
         }
     }
